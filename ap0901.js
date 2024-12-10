1eea641346f6a118ac90ea2ca784bf6557e3a4ef
238437 <134909165+238437@users.noreply.github.com>
2024-12-10 16:52:38 +0900

    自由課題
    ゴール追加

--- ap0901.js
+++ ap0901.js
@@ -49,7 +49,7 @@ function init() {
 
   // ボールの作成
   const ball = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 24, 24),
+    new THREE.SphereGeometry(1.2, 24, 24),
     new THREE.MeshBasicMaterial({ color: "yellow" })
   );
   ball.position.set(50, 0, 50);
@@ -121,44 +121,88 @@ function init() {
   scene.add(waku3);
 
   //ゴール
-  const go = new THREE.Mesh(
-    new THREE.BoxGeometry(5,2,7),
-    new THREE.MeshBasicMaterial({color: "red"})
-  );
-  go.position.set(48,0,0);
-  scene.add(go);
+  const gos = new THREE.Group();
+  {
+    const go = new THREE.Mesh(
+      new THREE.BoxGeometry(5,2,7),
+      new THREE.MeshBasicMaterial({color: "red"})
+    );
+    go.position.set(46,0,0);
+    go.rotation.y = -0.5 * Math.PI; // 角度を調整
+    go.geometry.computeBoundingBox();
+    gos.add(go);
+  }
+  scene.add(gos);
+  // ブロックの衝突検出
+  function brickCheck2() {
+    let hit = false;
+    const sphere = ball.geometry.boundingSphere.clone();
+    sphere.translate(ball.position);
+    gos.children.forEach((gos) => {
+      if (!hit ) {
+        let box = gos.geometry.boundingBox.clone();
+        box.translate(gos.position);
+        if (box.intersectsSphere(sphere)) {
+          
+          moveDirection = { x: 0, z: 0 };
+          hit = true;
+        }
+      }
+    });
+  }
 
   // 構造物２の作成（障壁）
   const kabes = new THREE.Group();
   {
     //壁
     const kabe = new THREE.Mesh(
-      new THREE.BoxGeometry(5,2,7),
+      new THREE.BoxGeometry(6,2,8),
       new THREE.MeshBasicMaterial({color: "black"})
     );
 
     //migi
     const migi = kabe.clone();
-    migi.position.set(48,0,15);
+    migi.position.set(36,0,15);
     migi.geometry.computeBoundingBox();
     kabes.add(migi);
     //hidari
     const hidari = kabe.clone();
-    hidari.position.set(22,0,15);
+    hidari.position.set(24,0,15);
     kabes.add(hidari);
     //i
     const hidari2 = kabe.clone();
-    hidari2.position.set(42,0,47);
+    hidari2.position.set(8.5,0,15);
+    hidari2.rotation.y = -0.5 * Math.PI; // 角度を調整
     kabes.add(hidari2);
+    //hidari3
+    const hidari3 = kabe.clone();
+    hidari3.position.set(40,0,45);
+    kabes.add(hidari3);
+
+    //u
+    for(let t=1; t<5; t++){
+      const u = kabe.clone();
+      u.position.set(-36,0,40+(-10*t));
+      kabes.add(u);
+    }
+
+    //u+
+    for(let t=1; t<5; t++){
+      const uu = kabe.clone();
+      uu.position.set(-36,0,-2+(-11*t));
+      uu.rotation.y = -0.5 * Math.PI; // 角度を調整
+      kabes.add(uu);
+    }
+    
 
     //tate+
     for (let c = 0; c < 3; c++) {
-      for (let r = 0; r < 5; r++) {
+      for (let r = 0; r < 3; r++) {
         const tate = kabe.clone();
         tate.position.set(
           18+(12*c),
           0,
-          -10+(12*c)*r
+          -15+(12*c)*r
         )
         kabes.add(tate);
       }
@@ -169,9 +213,9 @@ function init() {
       for (let r = 0; r < 8; r++) {
         const tate2 = kabe.clone();
         tate2.position.set(
-          -(18*c),
+          -1-(24*c),
           0,
-          30-(11*r)
+          42-(13*r)
         )
         kabes.add(tate2);
       }
@@ -182,9 +226,9 @@ function init() {
       for (let r = 0; r < 6; r++) {
         const yoko = kabe.clone();
         yoko.position.set(
-          -10+(24*c),
+          -10+(28*c),
           0,
-          18+(-12*r)
+          16+(-12*r)
         )
         yoko.rotation.y = -0.5 * Math.PI;//角度を調整
         kabes.add(yoko);
@@ -192,28 +236,16 @@ function init() {
     }
 
     //yoko-
-    for (let r = 0; r < 3; r++) {
+    for (let r = 0; r < 2; r++) {
       const yoko2 = kabe.clone();
       yoko2.position.set(
-        -24+(-12*r),
+        -16+(-12*r),
         0,
-        18+12*r
+        25+12*r
       )
       yoko2.rotation.y = -0.5 * Math.PI;//角度を調整
       kabes.add(yoko2);
     }
-
-    //yoko3
-    for (let r = 0; r < 3; r++) {
-      const yoko3 = kabe.clone();
-      yoko3.position.set(
-        6+(-12*r),
-        0,
-        24+12*r
-      )
-      kabes.add(yoko3);
-    }
-    
   }
   scene.add(kabes);
 
@@ -228,6 +260,7 @@ function init() {
         box.translate(kabes.position);
         if (box.intersectsSphere(sphere)) {
           ball.position.set(50,0,50);
+          moveDirection = { x: 0, z: 0 };
           hit = true;
         }
       }
@@ -244,7 +277,7 @@ function init() {
     // 次のフレームでの描画要請
     requestAnimationFrame(render);
     brickCheck(); // ブロックの衝突判定
-    
+    brickCheck2();
   }
 
   // 描画開始
